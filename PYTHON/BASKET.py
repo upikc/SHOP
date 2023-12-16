66ef495e4a55bfb9f4e3ef51630009469516143e
upik <116355193+upikc@users.noreply.github.com>
2023-12-16 09:41:40 +0500

    BASKET add dataWrite/sort, SortCBox

--- PYTHON/BASKET.py
+++ PYTHON/BASKET.py
@@ -3,31 +3,36 @@ from PyQt5.QtWidgets import *
 from DataContex import DataContex, Product_class
 import socket
 
+
 class ContentWindow(QWidget):
     def __init__(self, prodListAndUser: tuple):
         super().__init__()
+        self.SortCBox = QComboBox()
+        self.SortCBox.currentTextChanged.connect(lambda: self.dataWrite(self.SortCBox.currentIndex() != 0))
+        self.table = QTableWidget(0, 8)
         self.prodList, self.User = DataContex.tupleOfProdAndUser(prodListAndUser)
         self.initUI()
 
     def initUI(self):
         vbox = QVBoxLayout()
-        self.table = QTableWidget(0, 8)
+        HToolBox = QHBoxLayout()
 
         self.table.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
         self.table.setHorizontalHeaderLabels(
             ["Имя", "Описание", "ID_Cat", "ID_Creat", "Count_in_stock", "prise", "image",
              "Count_purchase"])
-
-        for j, i in enumerate(self.prodList):
-            self.writeRows(DataContex.deserializeProd(i), j)
+        self.SortCBox.addItems(["сортировать по возрастанию", "сортировать по убыванию"])
 
         vbox.addWidget(self.table)
-        vbox.addWidget(QLabel("сумма заказа :" + str(DataContex.purchaseAmount(self.prodList))))
+        vbox.addLayout(HToolBox)
+        HToolBox.addWidget(self.SortCBox)
+        HToolBox.addWidget(QLabel("сумма заказа :" + str(DataContex.purchaseAmount(self.prodList))))
         self.setLayout(vbox)
         self.setGeometry(300, 300, 920, 450)
 
     def writeRows(self, Product: Product_class, row):
-        self.table.insertRow(self.table.rowCount())
+        self.table.removeRow(row)
+        self.table.insertRow(row)
         self.table.setItem(row, 0, QTableWidgetItem(str(Product.Name)))
         self.table.setItem(row, 1, QTableWidgetItem(str(Product.Description)))
         self.table.setItem(row, 2, QTableWidgetItem(str(Product.Category_ID)))
@@ -36,6 +41,9 @@ class ContentWindow(QWidget):
         self.table.setItem(row, 5, QTableWidgetItem(str(Product.Prise)))
         self.table.setItem(row, 6, QTableWidgetItem(str(Product.Images)))
         self.table.setItem(row, 7, QTableWidgetItem(str(Product.Sold)))
-
         self.table.resizeColumnsToContents()
 
+    def dataWrite(self, reverse: bool):
+        self.prodList.sort(key=lambda x: int(x["Prise"]), reverse=reverse)
+        for j, i in enumerate(self.prodList):  # сортировать сюда
+            self.writeRows(DataContex.deserializeProd(i), j)
--- PYTHON/DataTest.py
+++ PYTHON/DataTest.py
@@ -3,10 +3,6 @@ from datetime import datetime
 
 import DataContex
 
-def add(a, b):
-    return a + b
-
-
 class Test(unittest.TestCase):
     def getTimeTest_Equal(self):
         self.assertEqual(DataContex.datetime, str(datetime.now().strftime("%m/%d/%Y, %H:%M:%S")))
